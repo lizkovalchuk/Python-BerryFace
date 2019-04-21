a0f67b4075c868894bb25d83400f0d7cd40b32ba
lizkovalchuk <liz.kovalchuk@gmail.com>
2019-04-20 21:39:21 -0400

    code to create random data

--- temp_humidity_04_2019.py
+++ temp_humidity_04_2019.py
@@ -2,11 +2,20 @@
 import sys
 from firebase import firebase
 import Adafruit_DHT
-from datetime import datetime
+from datetime import datetime, timedelta
+from random import randint
 
-humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)
 
+
+i = 0
 firebase = firebase.FirebaseApplication('https://raspberrypi-2019.firebaseio.com', None)
-result = firebase.post('/temperatures', temperature)
-result = firebase.post('/humidity', {'value': humidity, 'time': datetime.now().isoformat()})
-print result
+
+while True:
+	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)
+	temp_f = temperature + randint(-5,5)
+	hum_f = humidity + randint(-10,10)
+	minutes_to_substract = 30 * i
+	date = datetime.now() - timedelta(minutes=minutes_to_substract)
+	result = firebase.post('/records', {'timestamp': date.isoformat(), 'temperature': temp_f, "humidity" : hum_f})
+	i += 1
+
